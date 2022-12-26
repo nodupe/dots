c0b1dcf541784db3cfa24282309852bed78551df
Vinicius <viniciusxp@gmail.com>
2022-12-26 20:33:03 +0100

    Update run_once_before_00_install-package.sh

--- run_once_before_00_install-package.sh
+++ run_once_before_00_install-package.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -eu
-echo "${CHEZMOI_OS}"
+if [ "${CHEZMOI_OS}" == "linux" ]; then
 # Install age if it's not already installed
-if ! command -v age >/dev/null; then
+  if ! command -v age >/dev/null; then
   sudo apt update
   sudo apt install -y age
+  fi
 fi
-
