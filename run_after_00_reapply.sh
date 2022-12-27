6a94dc4b78af683476066509d5e506aea0fae2e7
Vinicius <viniciusxp@gmail.com>
2022-12-27 06:46:04 +0100

    Update run_after_00_reapply.sh

--- run_after_00_reapply.sh
+++ run_after_00_reapply.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -eu
-export PATH="${HOME}/.bin:${HOME}/.local/bin:${PATH}"
+export PATH="${HOME}/.bin:${HOME}/bin:${HOME}/.local/bin:${PATH}"
 if [ ! -f "${HOME}/.done" ]; then
 chezmoi purge --force
 touch "${HOME}/.done"
