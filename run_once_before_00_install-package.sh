82076a11dd298699f8f1beb78c6c040df4320523
Vinicius <viniciusxp@gmail.com>
2023-01-29 06:44:20 +0100

    Update run_once_before_00_install-package.sh
    Checking if we can use conditional to include gentoo

--- run_once_before_00_install-package.sh
+++ run_once_before_00_install-package.sh
@@ -5,10 +5,23 @@ export PATH="${HOME}/.bin:${HOME}/.local/bin:${HOME}/bin:${PATH}"
 echo 'PATH="${HOME}/.bin:${HOME}/.local/bin:${HOME}/bin:${PATH}"' >> ${HOME}/.bashrc
 
 
-if [ "${CHEZMOI_OS}" == "linux" ]; then
+if [ "${CHEZMOI_OSRELEASE_ID}" == "debian" ]; then
 # Install age if it's not already installed
   if ! command -v age >/dev/null; then
   sudo apt update
   sudo apt install -y age
+  fi
+fi
+if [ "${CHEZMOI_OSRELEASE_ID}" == "Ubuntu" ]; then
+# Install age if it's not already installed
+  if ! command -v age >/dev/null; then
+  sudo apt update
+  sudo apt install -y age
+  fi
+fi
+if [ "${CHEZMOI_OSRELEASE_ID}" == "Gentoo" ]; then
+# Install age if it's not already installed
+  if ! command -v age >/dev/null; then
+  sudo emerge age
   fi
 fi
